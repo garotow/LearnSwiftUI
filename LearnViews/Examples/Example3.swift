d68b965cb9eb00fce016497a8a67723eeb7492fb
Henrique Lima <henriquelima@tokenlab.com.br>
2020-01-15 09:35:31 -0300

    Improve example3

--- LearnViews/Examples/Example3.swift
+++ LearnViews/Examples/Example3.swift
@@ -13,8 +13,13 @@ struct Example3: View {
     var body: some View {
         ScrollView {
             Rectangle().fill(Color.red)
-                .frame(minWidth: 0.0, idealWidth: 100.0, maxWidth: 300.0, minHeight: 0.0, idealHeight: 100.0, maxHeight: 300.0)
+                .frame(minWidth: 0.0, idealWidth: 100.0, maxWidth: 300.0, minHeight: 0.0, idealHeight: 100.0, maxHeight: 600.0)
             Circle().fill(Color.green)
+            Text("Testando 123 texto meio longo talvez não caiba com muitas quebras de linha talvez nao caiba nada sera q muda tudo poq tem 3 linhas")
+                .frame(idealWidth: 100.0, maxWidth: 300.0, idealHeight: 70.0, maxHeight: 500)
+                .border(Color.red)
+                .fixedSize()
+                .border(Color.black)
 
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
